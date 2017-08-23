93303e4ed2b1798d6ffd5b82bd3afbb28e9172d5
JackWilkins <31279910+JackWilkins@users.noreply.github.com>
2017-08-23 15:17:22 +0100

    walls set
    set the postion of my walls

--- SchoolPlatformer 0.0.py
+++ SchoolPlatformer 0.0.py
@@ -4,16 +4,16 @@ black = (0,0,0)
 white = (255,255,255)
 green = (0,255,0)
 
-class Walls(pygame.sprite.Sprite):
+class Wall(pygame.sprite.Sprite):
     def __init__(self,x,y,width,height):
         pygame.sprite.Sprite.__Init__(self)
-        self.image = pygame.surface([width,height])
+        self.image = pygame.Surface([width,height])
         self.image.fill(green)
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
 
-class Player1(pygame.sprite.Sprie):
+class Player(pygame.sprite.Sprie):
     change_x = 0
     change_y = 0
     def __init__(self,x,y):
@@ -26,19 +26,55 @@ class Player1(pygame.sprite.Sprie):
         self.change_y +- y
     def update(self,nWall):
         self.rect.x += self.change_x
-        block_hit_list = pygame.sprite.spritecollide(self,nWall,False)
+        block_hit_list = pygame.sprite.spritecollide(self,walls,False)
         for block in block_hit_list:
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             else:
                 self.rect.left = block,rect.right
         self.rect.y +- self.change_y
-        block_hit_list = pygame.sprite.spritecollide(self,nWall,False)
+        block_hit_list = pygame.sprite.spritecollide(self,walls,False)
         for block in block_hit_list:
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             else:
                 self.rect.top = block.rect.bottom
+
+
+pygame.init()
+
+screen = pygame.display.set_mode([1024,768])
+screen = pygame.display.set_caption('Platformer')
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill(white)
+
+player = Player(30,30)
+
+all_sprites_list = pygame.sprite.Group()
+all_sprite_list.add(player)
+
+wall_list = pygame.sprite.Group()
+wall = Wall(0,0,15,768)
+wall_list.add(wall)
+all_sprite_list.add(wall)
+
+wall = Wall(15,0,1009,15)
+wall_list.add.(wall)
+all_sprite_list.add(wall)
+
+clock = pygame.time.Clock()
+
+done = False
+
+
+
+
+
+
+
+
 
 
         
